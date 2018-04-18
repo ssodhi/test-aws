f2ab76b8c00a540ac6a9b07ea1ea26d05e1b7e63
Shailendra Sodhi <s.sodhi@gns.cri.nz>
2018-04-18 15:54:19 +1200

    changes 7

--- tt/terraform.tf
+++ tt/terraform.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = "> 0.9.0"
 
   backend "s3" {
-    bucket  = "backups-dev.geonet.org.nz"
-    key     = "terraform/tf_aws/S3/backups.geonet.org.nz/shailendra-test-terraform.tfstate"
-    region  = "ap-southeast-2"
+    bucket = "backups-dev.geonet.org.nz"
+    key    = "terraform/tf_aws/S3/backups.geonet.org.nz/shailendra-test-terraform.tfstate"
+    region = "ap-southeast-2"
   }
 }
